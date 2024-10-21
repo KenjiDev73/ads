7e68085757d5f9ceb21e992d55a2239da4845b6c
MangkuPurel <106098930+KenjiDev73@users.noreply.github.com>
2024-10-21 11:11:55 +0700

    Create 320x50.js

---
+++ 320x50.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '9cb8ff6b1699a3e8faefbeaad28c1ba3',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//enginecorruptiontrice.com/9cb8ff6b1699a3e8faefbeaad28c1ba3/invoke.js"></script>
